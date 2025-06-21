47004fe00279f79082e26b505b0f0a642edd1e01
VitaliyPritula <pritula.vitaly94@gmail.com>
2025-06-21 16:30:00 +0300

    add contacts form

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,9 @@
-const express = require("express");
-const nodemailer = require("nodemailer");
-const cors = require("cors");
-require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
