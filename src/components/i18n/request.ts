94499be0d90368a0883a360dcd4376b3b458c2c8
Vitaly Pritula <v.prytula@sayorama.eu>
2026-05-27 09:35:52 +0300

    git commit -m "fix(i18n): migrate to latest next-intl API and resolve build issues"

--- src/components/i18n/request.ts
+++ src/components/i18n/request.ts
@@ -2,11 +2,19 @@ import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 import { routing } from './routing';
 
+type Locale = (typeof routing.locales)[number];
+
 export default getRequestConfig(async ({ locale }) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!routing.locales.includes(locale as any)) notFound();
+  const isValidLocale = (l: string | undefined): l is Locale => {
+    return !!l && (routing.locales as readonly string[]).includes(l);
+  };
+
+  if (!isValidLocale(locale)) {
+    notFound();
+  }
 
   return {
+    locale,
     messages: (await import(`../../messages/${locale}.json`)).default,
   };
 });
--- src/components/i18n/routing.ts
+++ src/components/i18n/routing.ts
@@ -1,5 +1,4 @@
 import { defineRouting } from 'next-intl/routing';
-import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 
 export const routing = defineRouting({
   // A list of all locales that are supported
@@ -9,7 +8,3 @@ export const routing = defineRouting({
   defaultLocale: 'ua',
 });
 
-// Lightweight wrappers around Next.js' navigation APIs
-// that will consider the routing configuration
-export const { Link, redirect, usePathname, useRouter } =
-  createSharedPathnamesNavigation(routing);
